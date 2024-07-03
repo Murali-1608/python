66cf0a1e777b4d9e02f3ffdb4d6d6f24eda380fd
Murali-1608 <120093915+Murali-1608@users.noreply.github.com>
2024-07-03 11:02:28 +0530

    Update Dict comprehension Practice.py

--- Dict comprehension Practice.py
+++ Dict comprehension Practice.py
@@ -32,15 +32,12 @@ print()
 A = dict(zip(list_of_name_sub_tot,[None]*len(list_of_name_sub_tot)))
 print(A)
 print()
-
 class_marks = {tuple_: total for tuple_, total in zip(list_of_name_sub_tot, total_marks)}
 print("class_marks =",class_marks)
 print()
-
 class_marks = {tuple_:[eng_marks[name],lang_marks[name],total]for name,tuple_,total in zip(names,list_of_name_sub_tot,total_marks)}
 print(class_marks)
 print()
-
 #7
 feedback = ["Kutty Pattas" if total > 120 else "Good Student" if total > 100 else "Slow Bloomer" for total in total_marks]
 class_marks = {(name, sub[0], sub[1], sub[2]): [eng_marks[name], lang_marks[name], total, fb]for name, total, fb in zip(names, total_marks, feedback)}
