b6653ebde56dd3afa4f263bc960a32713ea88b35
Murali-1608 <120093915+Murali-1608@users.noreply.github.com>
2024-08-08 17:16:01 +0530

    studentsnew.py

--- Mysql/students.py
+++ Mysql/students.py
@@ -1,7 +1,7 @@
 import mysql.connector as con
 db=con.connect(host="localhost",user="root",password="Password@16")
 cur=db.cursor()
-cur.execute("create database students")
+cur.execute("create database studentsnew")
 cur.execute("show databases")
 for i in cur:
     print(i)
